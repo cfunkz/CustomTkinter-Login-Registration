7ef77bda558d630fd9bfb8acb1aed9af261c0892
didis97 <116670695+didis97@users.noreply.github.com>
2023-09-28 19:00:00 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
 smtp_username = "davidsberzins1@gmail.com"  # Replace with your Gmail email address
-smtp_password = "plds mkgg tadp bdia"  # Replace with your Gmail password
+smtp_password = "aaaa"  # Replace with your Gmail password
 
 sender_email = "davidsberzins1@gmail.com"
