bc81fe44378a3afdce5cce44c10e256931d61151
Davids Berzins <116670695+cfunkz@users.noreply.github.com>
2023-12-27 01:17:44 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # SMTP server configuration
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
-smtp_username = "davidsberzins1@gmail.com"  # Replace with your Gmail email address
+smtp_username = "asd@asd.lv"  # Replace with your Gmail email address
 smtp_password = "aaaa"  # Replace with your Gmail password
 
-sender_email = "davidsberzins1@gmail.com"
+sender_email = "asd@asd.lv"
